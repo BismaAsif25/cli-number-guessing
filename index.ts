1be0cc04293603a6b3d6e7f4702421eaa337762e
BismaAsif25 <bismaasif455@gmail.com>
2024-04-02 04:32:37 -0700

    created cli number guessing game

---
+++ index.ts
@@ -0,0 +1,26 @@
+#! /usr/bin/env node
+
+import inquirer from "inquirer"
+
+const randomNumber = Math.floor(Math.random() * 6 +1 );
+
+
+const answers = await inquirer.prompt([
+    {
+        name:"userGuessingNumber",
+        type:"number",
+        message:"please guess a number between 1-6",
+    },
+]);
+
+if(answers.userGuessingNumber === randomNumber ) 
+{
+    console.log("congratulation ! you guessed the right number");
+}
+
+else
+{
+    console.log(" you guessed the wrong number");
+}
+
+
